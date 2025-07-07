78be71b663cacc7863b1855fe27cced0ab930d16
Mhammad Ibrahim <103114465+Mhammad2022Ibrahim@users.noreply.github.com>
2025-07-07 11:58:31 +0300

    try MLOPS

---
+++ tests/test_main.py
@@ -0,0 +1,31 @@
+from fastapi.testclient import TestClient
+from app.main import app
+import pytest
+
+client = TestClient(app)
+
+@pytest.mark.asyncio
+async def test_root():
+    response = await client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello in our async FastAPI app for named entity recognition!"}
+
+@pytest.mark.asyncio
+async def test_token_classification():
+    response = await client.post("/token_classification", json={"text": "OpenAI is in San Francisco."})
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) > 0
+    for entity in response.json():
+        assert "entity_group" in entity
+        assert "score" in entity
+        assert "word" in entity
+        assert "start" in entity
+        assert "end" in entity
+        
+        
+# def test_predict():
+#     response = client.post("/predict", json={"text": "OpenAI is in San Francisco."})
+#     assert response.status_code == 200
+#     assert "entities" in response.json()
+#     # assert isinstance(response.json()["entities"], list)
