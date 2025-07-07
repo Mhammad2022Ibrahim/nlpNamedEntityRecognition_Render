71cd7617443d513e2c3be8258d32e936938ae3d0
Mhammad Ibrahim <103114465+Mhammad2022Ibrahim@users.noreply.github.com>
2025-07-07 13:30:28 +0300

    try MLOPS

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,18 @@
-from fastapi.testclient import TestClient
-from app.main import app
 import pytest
-
-client = TestClient(app)
+from httpx import AsyncClient
+from app.main import app
 
 @pytest.mark.asyncio
 async def test_root():
-    response = await client.get("/")
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello in our async FastAPI app for named entity recognition!"}
 
 @pytest.mark.asyncio
 async def test_token_classification():
-    response = await client.post("/token_classification", json={"text": "OpenAI is in San Francisco."})
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.post("/token_classification", json={"text": "OpenAI is in San Francisco."})
     assert response.status_code == 200
     assert isinstance(response.json(), list)
     assert len(response.json()) > 0
@@ -22,10 +22,35 @@ async def test_token_classification():
         assert "word" in entity
         assert "start" in entity
         assert "end" in entity
-        
-        
-# def test_predict():
-#     response = client.post("/predict", json={"text": "OpenAI is in San Francisco."})
+
+# from fastapi.testclient import TestClient
+# from app.main import app
+# import pytest
+
+# client = TestClient(app)
+
+# @pytest.mark.asyncio
+# async def test_root():
+#     response = await client.get("/")
 #     assert response.status_code == 200
-#     assert "entities" in response.json()
-#     # assert isinstance(response.json()["entities"], list)
+#     assert response.json() == {"message": "Hello in our async FastAPI app for named entity recognition!"}
+
+# @pytest.mark.asyncio
+# async def test_token_classification():
+#     response = await client.post("/token_classification", json={"text": "OpenAI is in San Francisco."})
+#     assert response.status_code == 200
+#     assert isinstance(response.json(), list)
+#     assert len(response.json()) > 0
+#     for entity in response.json():
+#         assert "entity_group" in entity
+#         assert "score" in entity
+#         assert "word" in entity
+#         assert "start" in entity
+#         assert "end" in entity
+        
+        
+# # def test_predict():
+# #     response = client.post("/predict", json={"text": "OpenAI is in San Francisco."})
+# #     assert response.status_code == 200
+# #     assert "entities" in response.json()
+# #     # assert isinstance(response.json()["entities"], list)
