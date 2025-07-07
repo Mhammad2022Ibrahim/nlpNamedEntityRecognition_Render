97025afae1da9d42673c57a07ac1e08948330449
Mhammad Ibrahim <103114465+Mhammad2022Ibrahim@users.noreply.github.com>
2025-07-07 13:53:43 +0300

    try update MLOPS

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,6 +16,19 @@ async def test_root(async_client):
     assert response.status_code == 200
     assert response.json() == {"message": "Hello in our async FastAPI app for named entity recognition!"}
 
+@pytest.mark.asyncio
+async def test_token_classification(async_client):
+    response = await async_client.post("/token_classification", json={"text": "OpenAI is in San Francisco."})
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) > 0
+    for entity in response.json():
+        assert "entity_group" in entity
+        assert "score" in entity
+        assert "word" in entity
+        assert "start" in entity
+        assert "end" in entity
+
 # @pytest.mark.asyncio
 # async def test_token_classification():
 #     async with AsyncClient(app=app, base_url="http://test") as ac:
